36d5717927c8ce6d0d1a75f7bbbc630362819f19
comming-soon <himal9626@gmail.com>
2022-12-08 14:51:49 +0530

    Update parseService.js

--- src/services/parseService.js
+++ src/services/parseService.js
@@ -1,6 +1,6 @@
 const Parse = require('parse');
 Parse.initialize("myAppId", "YOUR_JAVASCRIPT_KEY");
 // Parse.serverURL = (process.env.NODE_ENV === "development" ? process.env.REACT_APP_SERVER_URL_DEV : process.env.REACT_APP_SERVER_URL_PROD);
-Parse.serverURL = "http://localhost:3031/parse";
+Parse.serverURL = "https://api-saas.aiensured.com/parse";
 
 export default Parse;
